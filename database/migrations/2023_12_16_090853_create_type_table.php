f1275cdf9b419162286557496373dabd6d84cad3
novadwiyanto <novadwynt28@gmail.com>
2023-12-16 16:24:46 +0700

    Make migration

--- database/migrations/2023_12_16_090853_create_type_table.php
+++ database/migrations/2023_12_16_090853_create_type_table.php
@@ -11,17 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('type', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone');
-            $table->unsignedBigInteger('room_id');
-            $table->rememberToken();
+            $table->integer('price');
+            $table->longText('description');
             $table->timestamps();
-
         });
     }
 
@@ -30,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('type');
     }
 };
--- database/migrations/2023_12_16_091053_create_rooms_table.php
+++ database/migrations/2023_12_16_091053_create_rooms_table.php
@@ -11,17 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('phone');
-            $table->unsignedBigInteger('room_id');
-            $table->rememberToken();
+            $table->unsignedBigInteger('type_id');
             $table->timestamps();
 
+            $table->foreign('type_id')->references('id')->on('type')->onDelete('restrict')->onUpdate('cascade');
         });
     }
 
@@ -30,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rooms');
     }
 };
